1002daeafa4ae03bb23b9361e63f4fe4fc7cff4f
dcpulido <dcpulido91@gmail.com>
2016-01-28 10:34:06 +0100

    casi finalizado codigo

--- reinas.pl
+++ reinas.pl
@@ -5,15 +5,25 @@ nreinas(N,Sol):-tablero(N,Tab),
 				per(Tab,Sol),
 				fin(Sol).
 
+%la fila la señala la posicion en el vector y la columna el valor 
 tablero(0,[]).
 tablero(N,[N|R]):-N1 is N-1,
-			  N1 > 0,
+			  N1 >= 0,
 			  tablero(N1,R).
 
+%permutaciones sobre el tablero
 per([],[]).
 per(X,[C|Cdr]):-selec(X,C,R),
 				per(R,Cdr).
 
-
+%
 selec([X|Cdr],X,Cdr).
 selec([C|Cdr],X,[C|R]):-selec(Cdr,X,R).
+
+%Compueba si ninguna reina puede comerse a otra
+fin([]).
+fin([X|Cdr]):-not(jaque(X,Cdr)),
+			  fin(Cdr).
+
+%Comprueba si una reina puede comerse a otra
+jaque()
